29e2d6d646e59e2a0cd00a4a9ad347dc16ad7bec
PenghaoJ <130850753+PenghaoJ@users.noreply.github.com>
2024-03-31 00:39:11 +1100

    update Weapons

--- SemesterProject/Assets/scripts/WeaponCondition.cs
+++ SemesterProject/Assets/scripts/WeaponCondition.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class WeaponCondition : MonoBehaviour
+public class PickupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text pickupText; // 拾取提示的UI Text组件
+    public float detectionRange = 3f; // 检测范围
+    public LayerMask interactableLayer; // 可互动物品所在的Layer
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+
+        // 从玩家的视角发出一条射线
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        // 检测射线是否碰撞到Layer为interactableLayer的物体
+        if (Physics.Raycast(ray, out hit, detectionRange, interactableLayer))
+        {
+            if (hit.collider.CompareTag("Weapon")) // 确保碰撞的物体标签为"Weapon"
+            {
+                pickupText.gameObject.SetActive(true); // 显示拾取提示
+            }
+            else
+            {
+                pickupText.gameObject.SetActive(false); // 隐藏拾取提示
+            }
+        }
+        else
+        {
+            pickupText.gameObject.SetActive(false); // 如果没有检测到物品，隐藏拾取提示
+        }
     }
 }
--- SemesterProject/Assets/scripts/WeaponPickup.cs
+++ SemesterProject/Assets/scripts/WeaponPickup.cs
@@ -6,12 +6,12 @@ public class WeaponPickup : MonoBehaviour
 {
     public GameObject weaponOnGround; // 地图上的武器
     public GameObject weaponInHand; // 主角手中的武器
-    public GameObject pickupText; // 提示文字的UI对象
+     // 提示文字的UI对象
 
     void Start()
     {
         weaponInHand.SetActive(false); // 确保游戏开始时主角手中的武器是隐藏的
-        pickupText.SetActive(false); // 确保游戏开始时拾取提示也是隐藏的
+         // 确保游戏开始时拾取提示也是隐藏的
     }
 
     // 当玩家进入触发区域时显示提示
@@ -19,7 +19,7 @@ public class WeaponPickup : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            pickupText.SetActive(true);
+            
         }
     }
 
@@ -28,7 +28,7 @@ public class WeaponPickup : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            pickupText.SetActive(false);
+            
         }
     }
 
@@ -39,7 +39,7 @@ public class WeaponPickup : MonoBehaviour
         {
             weaponOnGround.SetActive(false); // 隐藏地图上的武器
             weaponInHand.SetActive(true); // 显示主角手中的武器
-            pickupText.SetActive(false); // 拾取后隐藏提示文字
+            
         }
     }
 }
