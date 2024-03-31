b8f81007f804fc02df1440bfc03ff5d9ab68c0b1
PenghaoJ <130850753+PenghaoJ@users.noreply.github.com>
2024-03-31 19:09:00 +1100

    weapon

--- SemesterProject/Assets/scripts/Player.cs
+++ SemesterProject/Assets/scripts/Player.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int maxHealth = 100; // 主角的最大健康值
+    private int currentHealth; // 主角的当前健康值
+    private Animator animator; // 引用Animator组件
+
     void Start()
     {
-        
+        currentHealth = maxHealth; // 初始化时，主角的当前健康值等于最大健康值
+        animator = GetComponent<Animator>(); // 获取Animator组件
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 示例：使用按键触发行为和动画
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Run();
+        }
+
+        // 添加更多按键检测来触发其他行为和动画
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("Hurt"); // 触发受击动画
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack"); // 触发攻击动画
+    }
+
+    void Run()
+    {
+        animator.SetTrigger("Run"); // 触发跑步动画
+    }
+
+    // 添加其他行为的方法，例如Walk()和Die()
+
+    void Die()
+    {
+        animator.SetTrigger("Die"); // 触发死亡动画
+        // 实现死亡后的逻辑，例如禁用玩家控制
+        Debug.Log("Player died.");
     }
 }
--- SemesterProject/Assets/scripts/WeaponPickup.cs
+++ SemesterProject/Assets/scripts/WeaponPickup.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private GameObject weaponOnGround; // 地图上的武器
     private GameObject weaponInHand; // 主角手中的武器
 
@@ -19,6 +20,19 @@ public class WeaponPickup : MonoBehaviour
     }
 
     public void SetWeaponInHand(GameObject weapon)
+=======
+    public GameObject weaponOnGround; // 地图上的武器
+    public GameObject weaponInHand; // 主角手中的武器
+    //public GameObject pickupText; // 提示文字的UI对象
+
+    void Start()
+    {
+        weaponInHand.SetActive(false); // 确保游戏开始时主角手中的武器是隐藏的
+        //pickupText.SetActive(false); // 确保游戏开始时拾取提示也是隐藏的
+    }
+
+    private void OnTriggerEnter(Collider other)
+>>>>>>> Stashed changes
     {
         weaponInHand = weapon;
     }
@@ -29,6 +43,7 @@ public class WeaponPickup : MonoBehaviour
         // 检查触发器内的对象是否为玩家，并且玩家是否按下了F键
         if (other.CompareTag("Player"))
         {
+<<<<<<< Updated upstream
             // 隐藏地图上的武器
             weaponOnGround.SetActive(false);
 
@@ -37,3 +52,17 @@ public class WeaponPickup : MonoBehaviour
         }
     }
 }
+=======
+            //pickupText.SetActive(false); // 进入时可以选择隐藏提示文字
+            weaponOnGround.SetActive(false); // 隐藏地图上的武器   
+        }
+        weaponInHand.SetActive(true); // 显示主角手中的武器
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 当玩家离开触发区域时，如果你想要有一些额外的逻辑，可以在这里添加
+        weaponInHand.SetActive(true);
+    }
+}
+>>>>>>> Stashed changes
