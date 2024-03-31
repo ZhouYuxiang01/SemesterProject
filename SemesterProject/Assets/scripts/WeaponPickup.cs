bdef2dcccf175f58ef49f7c8744d9ae63915c1a9
PenghaoJ <130850753+PenghaoJ@users.noreply.github.com>
2024-03-31 18:58:37 +1100

    no idea what is this

--- SemesterProject/Assets/scripts/WeaponPickup.cs
+++ SemesterProject/Assets/scripts/WeaponPickup.cs
@@ -4,42 +4,36 @@ using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
-    public GameObject weaponOnGround; // 地图上的武器
-    public GameObject weaponInHand; // 主角手中的武器
-     // 提示文字的UI对象
+    private GameObject weaponOnGround; // 地图上的武器
+    private GameObject weaponInHand; // 主角手中的武器
 
     void Start()
     {
-        weaponInHand.SetActive(false); // 确保游戏开始时主角手中的武器是隐藏的
-         // 确保游戏开始时拾取提示也是隐藏的
+        weaponInHand.SetActive(false);
+        weaponOnGround = GameObject.FindWithTag("Interactable");
+        weaponInHand = GameObject.FindWithTag("Weapon");
+    }
+    public void SetWeaponOnGround(GameObject Interactable)
+    {
+        weaponOnGround = Interactable;
     }
 
-    // 当玩家进入触发区域时显示提示
-    private void OnTriggerEnter(Collider other)
+    public void SetWeaponInHand(GameObject weapon)
     {
+        weaponInHand = weapon;
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        // 检查触发器内的对象是否为玩家，并且玩家是否按下了F键
         if (other.CompareTag("Player"))
         {
-            
-        }
-    }
+            // 隐藏地图上的武器
+            weaponOnGround.SetActive(false);
 
-    // 当玩家离开触发区域时隐藏提示
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            
-        }
-    }
-
-    // 当玩家在触发区域内按下F键拾取武器时执行的逻辑
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.L))
-        {
-            weaponOnGround.SetActive(false); // 隐藏地图上的武器
-            weaponInHand.SetActive(true); // 显示主角手中的武器
-            
+            // 显示主角手中的武器
+            weaponInHand.SetActive(true);
         }
     }
 }
