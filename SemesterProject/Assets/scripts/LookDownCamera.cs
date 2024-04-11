d4eeacded95b84d61a57d67334da7b3ca1946610
Hastur0451 <zy20020220@gmail.com>
2024-04-11 16:04:55 +1000

    Use mouse control the camera

--- SemesterProject/Assets/scripts/LookDownCamera.cs
+++ SemesterProject/Assets/scripts/LookDownCamera.cs
@@ -36,14 +36,9 @@ public class LookDownCamera : MonoBehaviour
     {
         if (target != null)
         {
-            if (Input.GetKey(KeyCode.Q))
-            {
-                orbitAngle -= orbitSpeed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                orbitAngle += orbitSpeed * Time.deltaTime;
-            }
+            // 使用鼠标X轴移动来旋转相机
+            float mouseX = Input.GetAxis("Mouse X");
+            orbitAngle += mouseX * orbitSpeed * Time.deltaTime;
 
             Vector3 orbitDirection = Quaternion.Euler(0, orbitAngle, 0) * Vector3.back;
             Vector3 orbitPosition = target.position + orbitDirection * orbitDistance + Vector3.up * orbitHeight;
