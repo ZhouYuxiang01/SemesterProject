19b89e3d646d695cf22d42490e61bbc007a5b0a0
ZhouYuxiang01 <zyx6288@gmail.com>
2024-05-06 01:58:24 +1000

    update sword

---
+++ SemesterProject/Assets/WeaponModel.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+    private GameObject handweapon;
+    public GameObject weapon1;
+    public GameObject weapon2;
+    public GameObject weapon3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handweapon = null;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            weapon1.SetActive(false);
+            weapon2.SetActive(false);
+            weapon3.SetActive(false);
+        }
+        switch (other.gameObject.name)
+        {
+            case "30damage":
+                weapon1.SetActive(true);
+                other.gameObject.SetActive(false);
+                break;
+            case "60damage":
+                weapon2.SetActive(true);
+                other.gameObject.SetActive(false);
+                break;
+            case "100damage":
+                weapon3.SetActive(true);
+                other.gameObject.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+}
